6432d28dedbd50996dbd788f095894b2986c15f8
Nanchen Yang <ncyang@thoughtworks.com>
2017-02-27 22:36:59 +0800

    finished navigator to city list.

--- index.ios.js
+++ index.ios.js
@@ -4,58 +4,35 @@
  * @flow
  */
 import React, { Component } from 'react';
-import TabNavigator from 'react-native-tab-navigator';
-import TitleBar from './home.ios.js';
-import CityListScene from './cities.ios.js';
+import BaseScene from './src/base.js';
 
 import {
     AppRegistry,
     StyleSheet,
     Text,
     View,
-    Image
+    Image,
+    Navigator,
 } from 'react-native';
 
+const defaultRoute = {
+    component: BaseScene
+};
+
 export default class ReactNativeMeituan extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedTab: 'home'
-        };
+    _renderScene(route, navigator) {
+        let Component = route.component;
+        return (
+            <Component {...route.params} navigator={navigator} />
+        );
     }
 
     render() {
         return (
-            <TabNavigator>
-              <TabNavigator.Item
-                  selected={this.state.selectedTab === 'home'}
-                  title="首页"
-                  renderIcon={() => <Image source={require('./assets/images/ic_home_black_24dp.png')}/>}
-                  renderSelectedIcon={() => <Image source={require('./assets/images/ic_home_pressed_24dp.png')}/>}
-                  onPress={() => this.setState({selectedTab: 'home'})}>
-                  <TitleBar></TitleBar>
-              </TabNavigator.Item>
-              <TabNavigator.Item
-                  selected={this.state.selectedTab === 'nearby'}
-                  title="附近"
-                  renderIcon={() => <Image source={require('./assets/images/ic_pin_drop_black_24dp.png')}/>}
-                  renderSelectedIcon={() => <Image source={require('./assets/images/ic_pin_drop_pressed_24dp.png')}/>}
-                  onPress={() => this.setState({selectedTab: 'nearby'})}>
-                <View>
-                  <Text>Nearby</Text>
-                </View>
-              </TabNavigator.Item>
-              <TabNavigator.Item
-                  selected={this.state.selectedTab === 'profile'}
-                  title="我的"
-                  renderIcon={() => <Image source={require('./assets/images/ic_account_circle_24dp.png')}/>}
-                  renderSelectedIcon={() => <Image source={require('./assets/images/ic_account_circle_pressed_24dp.png')}/>}
-                  onPress={() => this.setState({selectedTab: 'profile'})}>
-                <View>
-                  <Text>Profile</Text>
-                </View>
-              </TabNavigator.Item>
-            </TabNavigator>
+            <Navigator
+                initialRoute={defaultRoute}
+                renderScene={this._renderScene} >
+            </Navigator>
         );
     }
 }
@@ -79,4 +56,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('ReactNativeMeituan', () => CityListScene);
+AppRegistry.registerComponent('ReactNativeMeituan', () => ReactNativeMeituan);
